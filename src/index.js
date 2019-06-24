438a251c7b78a961c52ce6baf72a96e1447e6007
github username <github email address>
2019-06-24 19:41:38 +0100

    customizing task routes

--- src/index.js
+++ src/index.js
@@ -25,3 +25,18 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+// const Task = require('./models/task')
+// const User = require('./models/user')
+
+// const main = async () => {
+//     // const task = await Task.findById('5d10a21d4a22b3443f3ac878')
+//     // await task.populate('owner').execPopulate()
+//     // console.log(task.owner)
+    
+//     const user = await User.findById ("5d10a14a4578c143e5d5af18")
+//     await user.populate('tasks').execPopulate()
+//     console.log(user.tasks)
+// }
+
+// main()
