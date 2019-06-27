12f4904049736e2125ad012588b2c3a672f957c3
github username <github email address>
2019-06-27 19:29:48 +0100

    refactored the express object called app into its own file, created tests using jest

---
+++ tests/user.test.js
@@ -0,0 +1,87 @@
+const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOneId = new mongoose.Types.ObjectId()
+const userOne = {
+    _id: userOneId,
+    name: "Johnny Bravo",
+    email: "bravo@example.com",
+    password: "johnnyBeGoode",
+    tokens: [{
+        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET )
+    }]
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
+
+test('Should sign up a new user', async () => {
+    const response = await request(app).post('/users').send({
+        name: 'daniel glover',
+        email: 'lovely@example.com',
+        password: 'swordpass'
+    }).expect(201)
+
+    // Assert that the database was changed correctly
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+
+    // Assertions about the response
+    expect(response.body).toMatchObject({
+        user: {
+            name: 'daniel glover',
+            age: 0,
+            email: 'lovely@example.com'
+        },
+        token: user.tokens[0].token
+    })
+})
+
+test('Should login exisiting user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Should login exisiting user', async () => {
+    await request(app).post('/users/login').send({
+        email: "failing@email.com",
+        password: "userOne.password"
+    }).expect(400)
+})
+
+test('Should get profile for user', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('Should not get profile for unauthenticated user', async () => {
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401)
+})
+
+test('Should delete account for user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('Should not delete account for unauthenticated user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .send()
+        .expect(401)
+})
